ea964fbdfcd16d455c1d8ca2937071e43eb98f9c
sravi4701 <sravi4701@gmail.com>
2017-07-07 13:21:38 +0530

    update settings activity

--- app/src/main/java/com/example/ravishankar/lapitchat/SettingsActivity.java
+++ app/src/main/java/com/example/ravishankar/lapitchat/SettingsActivity.java
@@ -7,6 +7,7 @@ import android.preference.PreferenceManager;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -94,56 +95,15 @@ public class SettingsActivity extends AppCompatActivity {
         mImageBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                //CropImage.activity().setGuidelines(CropImageView.Guidelines.ON).start(SettingsActivity.this);
-
-                Intent gallaryIntent = new Intent();
-                gallaryIntent.setType("image/*");
-                gallaryIntent.setAction(Intent.ACTION_GET_CONTENT);
-                startActivityForResult(Intent.createChooser(gallaryIntent, "SELECT IMAGE"), GALLARY_PIC);
+                CropImage.activity().setGuidelines(CropImageView.Guidelines.ON).start(SettingsActivity.this);
+//                Intent gallaryIntent = new Intent();
+//                gallaryIntent.setType("image/*");
+//                gallaryIntent.setAction(Intent.ACTION_GET_CONTENT);
+//                startActivityForResult(Intent.createChooser(gallaryIntent, "SELECT IMAGE"), GALLARY_PIC);
             }
         });
     }
 
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        if(requestCode == GALLARY_PIC && resultCode == RESULT_OK){
-
-            Uri  imageUri = data.getData();
-
-            CropImage.activity(imageUri).setAspectRatio(1,1)
-                    .start(this);
-            System.out.println("com");
-            if (requestCode == CropImage.CROP_IMAGE_ACTIVITY_REQUEST_CODE) {
-                System.out.println("coming");
-
-                CropImage.ActivityResult result = CropImage.getActivityResult(data);
-
-                if (resultCode == RESULT_OK) {
-
-                    Uri resultUri = result.getUri();
-                    Toast.makeText(SettingsActivity.this, resultUri.toString(), Toast.LENGTH_LONG).show();
-
-                    StorageReference filePath = mImageStorage.child("profile_images").child("profile_image.jpg");
-
-                    filePath.putFile(resultUri).addOnCompleteListener(new OnCompleteListener<UploadTask.TaskSnapshot>() {
-                        @Override
-                        public void onComplete(@NonNull Task<UploadTask.TaskSnapshot> task) {
-                            if(task.isSuccessful()){
-                                Toast.makeText(SettingsActivity.this, "Success", Toast.LENGTH_LONG).show();
-                            }
-                            else{
-                                Toast.makeText(SettingsActivity.this, "Got Some error", Toast.LENGTH_LONG).show();
-                            }
-                        }
-                    });
-
-                } else if (resultCode == CropImage.CROP_IMAGE_ACTIVITY_RESULT_ERROR_CODE) {
-                    Exception error = result.getError();
-                }
-            }
-        }
-    }
     public static String random() {
         Random generator = new Random();
         StringBuilder randomStringBuilder = new StringBuilder();
@@ -156,3 +116,48 @@ public class SettingsActivity extends AppCompatActivity {
         return randomStringBuilder.toString();
     }
 }
+
+
+//    @Override
+//    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+//        super.onActivityResult(requestCode, resultCode, data);
+//        if(requestCode == GALLARY_PIC && resultCode == RESULT_OK){
+//
+//            Uri  imageUri = data.getData();
+//
+//            CropImage.activity(imageUri).setAspectRatio(1,1)
+//                    .start(this);
+//            Log.v("message: ", "ok till 117");
+//
+//            Log.v("message: ", Integer.toString(requestCode));
+//            Log.v("message: ", Integer.toString(CropImage.CROP_IMAGE_ACTIVITY_REQUEST_CODE));
+//            if (requestCode == CropImage.CROP_IMAGE_ACTIVITY_REQUEST_CODE) {
+//                Log.v("message: ", "ok till 119");
+//
+//                CropImage.ActivityResult result = CropImage.getActivityResult(data);
+//
+//                if (resultCode == RESULT_OK) {
+//
+//                    Uri resultUri = result.getUri();
+//                    Toast.makeText(SettingsActivity.this, resultUri.toString(), Toast.LENGTH_LONG).show();
+//
+//                    StorageReference filePath = mImageStorage.child("profile_images").child("profile_image.jpg");
+//
+//                    filePath.putFile(resultUri).addOnCompleteListener(new OnCompleteListener<UploadTask.TaskSnapshot>() {
+//                        @Override
+//                        public void onComplete(@NonNull Task<UploadTask.TaskSnapshot> task) {
+//                            if(task.isSuccessful()){
+//                                Toast.makeText(SettingsActivity.this, "Success", Toast.LENGTH_LONG).show();
+//                            }
+//                            else{
+//                                Toast.makeText(SettingsActivity.this, "Got Some error", Toast.LENGTH_LONG).show();
+//                            }
+//                        }
+//                    });
+//
+//                } else if (resultCode == CropImage.CROP_IMAGE_ACTIVITY_RESULT_ERROR_CODE) {
+//                    Exception error = result.getError();
+//                }
+//            }
+//        }
+//    }
