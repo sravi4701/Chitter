faa20f2f7c2106308e3d1b1ed5185d6f796c737f
sravi4701 <sravi4701@gmail.com>
2017-07-10 19:57:38 +0530

    Finish AllUsersActivity

---
+++ app/src/main/java/com/example/ravishankar/lapitchat/Users.java
@@ -0,0 +1,44 @@
+package com.example.ravishankar.lapitchat;
+
+/**
+ * Created by ravishankar on 10/7/17.
+ */
+
+public class Users {
+    public String name;
+    public String image;
+    public String status;
+
+    public Users(){
+
+    }
+    public Users(String name, String image, String status) {
+        this.name = name;
+        this.image = image;
+        this.status = status;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+}
--- app/src/main/java/com/example/ravishankar/lapitchat/UsersActivity.java
+++ app/src/main/java/com/example/ravishankar/lapitchat/UsersActivity.java
@@ -2,12 +2,20 @@ package com.example.ravishankar.lapitchat;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.TextView;
+
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class UsersActivity extends AppCompatActivity {
     private Toolbar mToolbar;
     private RecyclerView mUsersList;
+    private DatabaseReference mUsersDatabase;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,7 +26,48 @@ public class UsersActivity extends AppCompatActivity {
         getSupportActionBar().setTitle("All Users");
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
-        mUsersList = (RecyclerView) findViewById(R.id.users_list);
+        mUsersDatabase = FirebaseDatabase.getInstance().getReference().child("Users");
 
+        mUsersList = (RecyclerView) findViewById(R.id.users_list);
+        mUsersList.setHasFixedSize(true);
+        mUsersList.setLayoutManager(new LinearLayoutManager(this));
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        FirebaseRecyclerAdapter<Users, UsersViewHolder> firebaseRecyclerAdapter = new FirebaseRecyclerAdapter<Users, UsersViewHolder>(
+                Users.class,
+                R.layout.users_single_layout,
+                UsersViewHolder.class,
+                mUsersDatabase
+        ) {
+            @Override
+            protected void populateViewHolder(UsersViewHolder viewHolder, Users model, int position) {
+                viewHolder.setName(model.getName());
+                viewHolder.setStatus(model.getStatus());
+            }
+        };
+
+        mUsersList.setAdapter(firebaseRecyclerAdapter);
+    }
+
+    public static class UsersViewHolder extends RecyclerView.ViewHolder {
+        private View mView;
+        public UsersViewHolder(View itemView) {
+            super(itemView);
+            mView = itemView;
+        }
+
+        public void setName(String name){
+            TextView userNameView= (TextView)mView.findViewById(R.id.users_single_name);
+            userNameView.setText(name);
+        }
+        public void setStatus(String status){
+            TextView userStatusView = (TextView)mView.findViewById(R.id.users_single_status);
+            userStatusView.setText(status);
+        }
     }
 }
